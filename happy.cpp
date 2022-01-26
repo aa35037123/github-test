61ac53daeb90973f1e2eddaf36a51271490e3a8d
aa35037123 <wesleyshi0321@gmail.com>
2022-01-26 22:22:12 +0800

    itron winter homework

---
+++ happy.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Wesley Shi on 2022/1/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
